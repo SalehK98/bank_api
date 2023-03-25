20a92def574f46f98924ff22b6457aa956d96a27
Saleh Kalouti <salehkalouti@gmail.com>
2023-03-25 16:46:22 +0300

    refactor routes-connected to controllers

--- server/routes/user/Routes.js
+++ server/routes/user/Routes.js
@@ -1,30 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const userControllers = require("../../controllers/userController");
 
-router
-  .route("/")
-  .get((req, res) => {
-    console.log("get request");
-    res.end("get all users");
-  })
-  .post((req, res) => {
-    console.log("post request");
-    res.end("added a user");
-  });
+router.route("/").get(userControllers.getUsers).post(userControllers.addUser);
 
 router
   .route("/:id")
-  .get((req, res) => {
-    console.log("get request");
-    res.end(`get a user ${req.params.id}`);
-  })
-  .put((req, res) => {
-    console.log("put request");
-    res.end(`update a user ${req.params.id}`);
-  })
-  .delete((req, res) => {
-    console.log("delete request");
-    res.end(`delete a user ${req.params.id}`);
-  });
+  .get(userControllers.getUser)
+  .put(userControllers.updateUser)
+  .delete(userControllers.deleteUser);
 
 module.exports = router;
